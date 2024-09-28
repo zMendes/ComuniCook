4bf37be0b9dae8197b6690f164a083a4cb7d2bfb
zMendes <leonardom1808@gmail.com>
2024-09-28 13:11:21 -0300

    add fkng missing restaurant file

---
+++ restaurant.py
@@ -0,0 +1,21 @@
+class Restaurant:
+    def __init__(self, plate_tables):
+        self.money = 0
+        self.foods = ["egg"]
+        self.queue = []
+        self.plate_tables = plate_tables
+    def get_money(self):
+        return self.money
+    def get_top_queue(self):
+        print(len(self.queue[-5:]))
+        return self.queue[-5:]
+    def add_to_queue(self, person):
+        print("added to queue")
+        self.queue.append(person)
+    def give_food(self):
+        for plate_table in self.plate_tables:
+            if plate_table.has_food():
+                hungry = self.queue.pop(0)
+                hungry.eat(plate_table.get_food())
+    def update(self, delta_time):
+        self.give_food()
